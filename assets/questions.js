79c06e667766f18da265a2a4cafedc2a34601808
pizza <bluetoothdoof@proton.me>
2024-07-27 02:26:01 -0300

    made basic quiz

---
+++ assets/questions.js
@@ -0,0 +1,429 @@
+const questions = [
+    {
+        text: "Você já falou alguma coisa sem pensar nas consequências antes?", answers: [
+            { text: "Sim.", points: { lonely: 4, rash: 4 } },
+            { text: "Não.", points: { hardy: 2 } }
+        ]
+    },
+    {
+        text: "Você gostaria de ser mais alto ou alta?", answers: [
+            { text: "Pra caralho!", points: { sassy: 4 } },
+            { text: "Claro que não!", points: { calm: 2 } }
+        ]
+    },
+    {
+        text: "Uma vez que você decide alguma coisa, você acha que tem que seguir com ela até o final?", answers: [
+            { text: "Sim.", points: { hardy: 4 } },
+            { text: "Não.", points: { quirky: 4, hasty: 2 } }
+        ]
+    },
+    {
+        text: "Você á falou \"Prazer em te conhecer\" para alguém que já tinha conhecido antes?", answers: [
+            { text: "Sim.", points: { quirky: 4, rash: 4 } },
+            { text: "Não.", points: { calm: 2 } }
+        ]
+    },
+    {
+        text: "Você está no cinema. Que filme você foi ver?", answers: [
+            { text: "Um filme de ação.", points: { impish: 4, sassy: 2 } },
+            { text: "Um drama.", points: { hardy: 4 } },
+            { text: "Um romance.", points: { quirky: 4 } },
+        ]
+    },
+    {
+        text: "Já olhou pra sua reflexão e pensou \"Que delícia\"?", answers: [
+            { text: "Óbvio!", points: { jolly: 2, naive: 2, sassy: 4 } },
+            { text: "Bem, não ...", points: { hardy: 2 } }
+        ]
+    },
+    {
+        text: "Já pensou que se você cavar no seu quintal você pode achar tesouro escondido?", answers: [
+            { text: "Sim.", points: { naive: 4 } },
+            { text: "Não.", points: { quiet: 2 } }
+        ]
+    },
+    {
+        text: "Você prefere brincar dentro ou fora de casa?", answers: [
+            { text: "Lá fora", points: { impish: 4, rash: 4 } },
+            { text: "Dentro", points: { calm: 2 } }
+        ]
+    },
+    {
+        text: "Você descobre um baú velho em algumas ruínas. O que você faz?", answers: [
+            { text: "Eu abro, ué!", points: { brave: 4, hasty: 4, impish: 4, rash: 2 } },
+            { text: "Chamo alguém pra me ajudar a abrir.", points: { timid: 2 } }
+        ]
+    },
+    {
+        text: "Já teve uma vez que você se tocou que estava falando demais?", answers: [
+            { text: "Sim.", points: { rash: 4, hasty: 4 } },
+            { text: "Não.", points: { docile: 2, quiet: 2 } }
+        ]
+    },
+    {
+        text: "Quando você vê um botão, você sente uma vontade avassaladora de apertar ele?", answers: [
+            { text: "Sim.", points: { hasty: 4 } },
+            { text: "Não.", points: { calm: 2 } },
+        ]
+    },
+    {
+        text: "Já se esqueceu que comprou alguma coisa e comprou ela de novo?", answers: [
+            { text: "Sim.", points: { hasty: 2, quirky: 4, rash: 2 } },
+            { text: "Não.", points: { quiet: 2 } }
+        ]
+    },
+    {
+        text: "Você acha que é importante sempre tentar ser o número 1?", answers: [
+            { text: "Claro!", points: { lonely: 2, sassy: 4 } },
+            { text: "Não.", points: { calm: 4, quirky: 2 } }
+        ]
+    },
+    {
+        text: "Você quer ser famoso ou famosa?", answers: [
+            { text: "Sim.", points: { lonely: 4, sassy: 4 } },
+            { text: "Não.", points: { relaxed: 2 } }
+        ]
+    },
+    {
+        text: "Se você visse alguém fazendo merda, você acha que deve chamar a atenção dessa pessoa?", answers: [
+            { text: "Claro!", points: { bold: 2, brave: 4, sassy: 4 } },
+            { text: "Não.", points: { timid: 2 } }
+        ]
+    },
+    {
+        text: "Você já contou uma piada que ninguém riu?", answers: [
+            { text: "Sim.", points: { impish: 2, naive: 4 } },
+            { text: "Não.", points: { calm: 4 } }
+        ]
+    },
+    {
+        text: "Você gosta de festas animadas?", answers: [
+            { text: "Sim.", points: { jolly: 4, lonely: 2 } },
+            { text: "Não.", points: { quiet: 2 } }
+        ]
+    },
+    {
+        text: "Quando você se desculpa, você é realmente sincero ou sincera?", answers: [
+            { text: "Claro!", points: { bold: 2, docile: 4 } },
+            { text: "Não é fácil admitir isso!", points: { lonely: 2, timid: 4 } }
+        ]
+    },
+    {
+        text: "Você gosta de karaokê?", answers: [
+            { text: "Sim.", points: { jolly: 4, bold: 4 } },
+            { text: "Não.", points: { docile: 2, timid: 2 } }
+        ]
+    },
+    {
+        text: "Você tá subindo uma trilha quando você chega em uma bifurcação. Qual caminho você escolhe?", answers: [
+            { text: "Estreito", points: { impish: 4, naive: 2 } },
+            { text: "Largo", points: { quirky: 4, timid: 2 } }
+        ]
+    },
+    {
+        text: "Um amigo leva um tombo enorme! O que você faz?", answers: [
+            { text: "Ajudo ele a se levantar!", points: { brave: 4, lonely: 2 } },
+            { text: "Rio! Engraçado pra caralho!", points: { impish: 4, naive: 2, rash: 2 } }
+        ]
+    },
+    {
+        text: "Você tem uma prova importante amanhã! O que você faz?", answers: [
+            { text: "Viro a noite estudando.", points: { hardy: 4 } },
+            { text: "Improviso! Tenho certeza que vai dar bom.", points: { relaxed: 4 } },
+            { text: "Prova? Acho que tô com febre ...", points: { naive: 4 } }
+        ]
+    },
+    {
+        text: "Você alguma vez já chamou um professor ou professora de \"Pai\" ou \"Mãe\"?", answers: [
+            { text: "Sim.", points: { rash: 4, bold: 2 } },
+            { text: "Não.", points: { quiet: 2 } }
+        ]
+    },
+    {
+        text: "Tem um rumor no bairro sobre um fantasma no banheiro da escolha! O que você faz?", answers: [
+            { text: "Que medo ... banheiros!", points: { docile: 4, timid: 2 } },
+            { text: "Vou como normal.", points: { bold: 2, jolly: 4, relaxed: 2 } }
+        ]
+    },
+    {
+        text: "Você está em aula quando vê que tem que ir no banheiro! O que você faz?", answers: [
+            { text: "Peço permissão pra ir.", points: { brave: 4, bold: 4 } },
+            { text: "Saio de fininho.", points: { hasty: 2 } },
+            { text: "Me seguro até o fim da aula.", points: { timid: 2 } }
+        ]
+    },
+    {
+        text: "Você acha que tem bons hábitos de estudo?", answers: [
+            { text: "Sim.", points: { docile: 4 } },
+            { text: "Não.", points: { naive: 4, rash: 2 } },
+            { text: "Tenho histórico de atleta!", points: { impish: 4 } }
+        ]
+    },
+    {
+        text: "Qual é o seu estilo de estudo?", answers: [
+            { text: "Trabalho duro, todo dia", points: { hardy: 4 } },
+            { text: "Se eu me lembrar...", points: { quirky: 4, hasty: 2 } },
+            { text: "Eu dou uma olhada no que é necessário.", points: { quiet: 2 } },
+            { text: "Não consigo sem ajuda...", points: { lonely: 4 } }
+        ]
+    },
+    {
+        text: "Você está preparando comida pra um piquenique com sua turma quando fica com fome. O que você faz?", answers: [
+            { text: "Como um pouquinho.", points: { hasty: 4 } },
+            { text: "Me seguro e termino de preparar.", points: { hardy: 4 } },
+            { text: "Que comida? Já comi.", points: { rash: 4 } }
+        ]
+    },
+    {
+        text: "Você vê um bolo que venceu ontem. O que você faz?", answers: [
+            { text: "Não é um problema! Vai pra dentro!", points: { brave: 4, relaxed: 2 } },
+            { text: "Penso nisso por um segundo e decido.", points: { timid: 2 } },
+            { text: "Peço pra alguém decidir antes.", points: { bold: 4 } }
+        ]
+    },
+    {
+        text: "Você comeu até se fartar quando uma sobremesa deliciosa aparece. O que você faz?", answers: [
+            { text: "Como. Meu estômago é que nem coração de mãe.", points: { hasty: 4, rash: 2 } },
+            { text: "Recuso. Engorda demais!", points: { hardy: 2 } },
+            { text: "Delícia! Adoro sobremesa! ♪", points: { bold: 4, jolly: 4, relaxed: 2 } }
+        ]
+    },
+    {
+        text: "Seu amigo fez uma comida horrível. Ele te pergunta \"O que achou?\" O que você diz ..?", answers: [
+            { text: "\"Foi uma merda\"", points: { brave: 2, quiet: 2 } },
+            { text: "Só sorrio", points: { calm: 4, lonely: 2 } },
+            { text: "\"Ehhh, é ... bom.\"", points: { rash: 2, timid: 2 } }
+        ]
+    },
+    {
+        text: "Você vai comer em um restaurante chique conhecido pela comida. Que proteína escolhe?", answers: [
+            { text: "Carne vermelha.", points: { impish: 4, naive: 4 } },
+            { text: "Carne branca ou proteína vegetal", points: { hardy: 2 } },
+            { text: "Qualquer coisa! Tudo é uma delícia!", points: { bold: 4 } }
+        ]
+    },
+    {
+        text: "Todo mundo tá dividindo uma sobremesa e tem uma fatia sobrando. O que você faz?", answers: [
+            { text: "Não conto pra ninguém.", points: { bold: 4, timid: 2 } },
+            { text: "Conto pra todo mundo.", points: { docile: 4, rash: 4 } },
+            { text: "É de quem pegar primeiro.", points: { impish: 4 } }
+        ]
+    },
+    {
+        text: "Um amigo te oferece um jantar à conta dele. O que você faz?", answers: [
+            { text: "Já tô lá!", points: { bold: 4 } },
+            { text: "Se não for incomodar", points: { jolly: 4 } },
+            { text: "Obrigado...", points: { quirky: 4 } }
+        ]
+    },
+    {
+        text: "Todo mundo ao seu redor está rindo mundo de algo que você acha chato. O que você faz?", answers: [
+            { text: "Nada.", points: { brave: 4, bold: 2 } },
+            { text: "Rio junto.", points: { lonely: 4, timid: 2 } },
+            { text: "Depende da situação.", points: { quiet: 2, quirky: 2 } }
+        ]
+    },
+    {
+        text: "Você prefere uma agenda cronometrada ou muito tempo vago?", answers: [
+            { text: "Agenda cronometrada", points: { lonely: 4, sassy: 2 } },
+            { text: "Tempo livre!", points: { calm: 4 } },
+            { text: "Meio termo!", points: { quirky: 4 } }
+        ]
+    },
+    {
+        text: "Você está prestes a comprar um jogo popular quando alguém compra a última cópia! Como se sente?", answers: [
+            { text: "Que seja!", points: { calm: 4 } },
+            { text: "Puto. Eu tava aqui primeiro.", points: { jolly: 4, relaxed: 4 } },
+            { text: "Choro um pouco.", points: { lonely: 4 } }
+        ]
+    },
+    {
+        text: "Você encontra uma pessoa com a qual não tinha falado muito antes. O que você faz?", answers: [
+            { text: "Faço conversa fiada.", points: { calm: 2 } },
+            { text: "Digo nada!", points: { quirky: 2 } },
+            { text: "Dou uma desculpa pra ir embora!", points: { timid: 4 } }
+        ]
+    },
+    {
+        text: "Boas notícias e más notícias... Qual quer ouvir primeiro?", answers: [
+            { text: "Boas notícias", points: { relaxed: 4 } },
+            { text: "Más notícias", points: { bold: 2, naive: 4 } },
+            { text: "Não quero ouvir nada", points: { timid: 4 } }
+        ]
+    },
+    {
+        text: "Te entregaram uma caixa grande como um souvenir. O que você faz?", answers: [
+            { text: "Você espera chegar em casa pra abrir.", points: { docile: 4 } },
+            { text: "Balança ela!", points: { naive: 4 } },
+            { text: "Abre logo!", points: { rash: 4 } },
+            { text: "É menor que a dos outros ...", points: { lonely: 4 } }
+        ]
+    },
+    {
+        text: "Hey, o que é isso? Tem alguém atrás de você! Então ... você olhou pra trás?", answers: [
+            { text: "Não faz isso comigo! Quase me matou de susto!", points: { timid: 4 } },
+            { text: "Ok, admito que sim. Você me tapeou.", points: { docile: 4 } },
+            { text: "Não.", points: { lonely: 4, sassy: 4 } },
+            { text: "Quê?", points: { relaxed: 4 } }
+        ]
+    },
+    {
+        text: "Como são suas manhãs?", answers: [
+            { text: "Sempre uma correria!", points: { brave: 4, impish: 4 } },
+            { text: "Sempre perfeitas!", points: { quiet: 2 } },
+            { text: "São OK.", points: { docile: 2, timid: 4 } }
+        ]
+    },
+    {
+        text: "Te convidaram pra uma festa de arromba. Na hora da festa começar não tem ninguém lá! O que você pensa?", answers: [
+            { text: "Aconteceu alguma coisa?", points: { docile: 4, lonely: 4 } },
+            { text: "Vim no dia errado?", points: { jolly: 4, relaxed: 4 } },
+            { text: "Vamos animar essa festa!", points: { bold: 4 } }
+        ]
+    },
+    {
+        text: "Você tira seus sapatos e se toca que suas meias são de cores diferentes! O que você faz?", answers: [
+            { text: "Morro de vergonha!", points: { docile: 4 } },
+            { text: "De novo !?", points: { rash: 4 } },
+            { text: "Foi de propósito!", points: { jolly: 4 } }
+        ]
+    },
+    {
+        text: "Você faz Promessas de Ano Novo?", answers: [
+            { text: "Claro!", points: { hardy: 4 } },
+            { text: "Não.", points: { quirky: 4, rash: 2 } },
+            { text: "Quê que é isso?", points: { relaxed: 4 } }
+        ]
+    },
+    {
+        text: "Você tá em uma reunião quieta quando escuta alguém peidar. Como você reage?", answers: [
+            { text: "Morro de rir!", points: { jolly: 4, naive: 4 } },
+            { text: "Deixo quieto...", points: { docile: 2, impish: 2 } },
+            { text: "Quem fez isso?", points: { brave: 4 } }
+        ]
+    },
+    {
+        text: "Você tá em uma maratona, e no começo você leva um tombo! O que você faz?", answers: [
+            { text: "Não vou desistir!", points: { brave: 2, hardy: 4 } },
+            { text: "Desisto.", points: { quirky: 4 } },
+            { text: "Grito \"Começa de novo!\"", points: { bold: 4, rash: 2 } }
+        ]
+    },
+    {
+        text: "Você encontra algo em uma pechincha! O que você faz?", answers: [
+            { text: "Compro logo!", points: { hasty: 4 } },
+            { text: "Penso se eu preciso ou não.", points: { quiet: 2 } },
+            { text: "Exijo um desconto ainda maior!", points: { bold: 4 } }
+        ]
+    },
+    {
+        text: "Te pediram pra fazer uma tarefa difícil. O que você vai fazer?", answers: [
+            { text: "Fazer sozinho.", points: { brave: 2, hardy: 4 } },
+            { text: "Pedir ajuda.", points: { docile: 2 } },
+            { text: "Fazer outra pessoa fazer!", points: { bold: 4, sassy: 2 } }
+        ]
+    },
+    {
+        text: "Você nota que um presente que você comprou ontem tá em 50% de desconto hoje. Como você se sente?", answers: [
+            { text: "Coração partido...", points: { hardy: 4 } },
+            { text: "AAAAAAAAAAHHHHHHHH", points: { docile: 4 } },
+            { text: "Timing ruim, né...", points: { bold: 4, calm: 4 } }
+        ]
+    },
+    {
+        text: "Seu amigo tá meio atrasado pra te encontrar. Isso é OK?", answers: [
+            { text: "Sim.", points: { bold: 4, relaxed: 2 } },
+            { text: "Não.", points: { hasty: 2, lonely: 4 } }
+        ]
+    },
+    {
+        text: "Você acha que, não importa o que acontecer, a vida continua?", answers: [
+            { text: "Claro!", points: { jolly: 2, relaxed: 4 } },
+            { text: "Nunca.", points: { quiet: 2 } }
+        ]
+    },
+    {
+        text: "O telefone tá tocando! O que você faz?", answers: [
+            { text: "Atendo logo!", points: { hasty: 4, lonely: 4 } },
+            { text: "Espero um pouco antes de responder", points: { quiet: 2 } },
+            { text: "Ignoro e deixo tocar", points: { timid: 2 } }
+        ]
+    },
+    {
+        text: "Seus amigos parecem estar planejando alguma coisa de onde você não consegue ouvir. Você pensa ...", answers: [
+            { text: "Quero ir também!", points: { naive: 4 } },
+            { text: "Nada demais.", points: { lonely: 4 } },
+            { text: "Será que eles vão me convidar?", points: { timid: 2 } }
+        ]
+    },
+    {
+        text: "Você acha que é importante ser pontual?", answers: [
+            { text: "Não.", points: { brave: 4 } },
+            { text: "Sim.", points: { bold: 4, quiet: 4 } },
+            { text: "Sei lá.", points: { docile: 4 } }
+        ]
+    },
+    {
+        text: "Você ganhou muito em uma rifa! O que você diz?", answers: [
+            { text: "Aeeeee porra!", points: { jolly: 2, naive: 4 } },
+            { text: "Não consigo acreditar...", points: { timid: 2 } },
+            { text: "Esse é o meu segredinho...", points: { quiet: 4 } }
+        ]
+    },
+    {
+        text: "Você acha que culpar alguém por algo que você fez é necessário de vez em quando?", answers: [
+            { text: "Claro!", points: { quiet: 4, sassy: 4 } },
+            { text: "Não!", points: { brave: 4 } }
+        ]
+    },
+    {
+        text: "Seu amigo do nada para de falar com você, quando ontem tava tudo bem. O que aconteceu?", answers: [
+            { text: "Talvez ele esteja doente.", points: { hasty: 4 } },
+            { text: "Ele tá ocupado.", points: { quiet: 4 } },
+            { text: "O quê! Por quê?", points: { timid: 2 } }
+        ]
+    },
+    {
+        text: "Você falhou miseravelmente. Todo mundo descobriu, e estão decepcionados com você ... O que você faz?", answers: [
+            { text: "Tenta se esconder.", points: { timid: 2 } },
+            { text: "Pensa em uma desculpa.", points: { naive: 4, sassy: 2 } },
+            { text: "Finge que morreu.", points: { rash: 4 } }
+        ]
+    },
+    {
+        text: "Você quer mostrar que você gosta muito de uma certa pessoa! O que você faz?", answers: [
+            { text: "Mostro um pouco jogando algo junto.", points: { jolly: 4, quiet: 2 } },
+            { text: "Deixo óbvio ao ... pregar uma peça!", points: { lonely: 4, naive: 2 } },
+            { text: "Falo logo pra todo mundo!", points: { bold: 2, brave: 4, impish: 4 } },
+            { text: "Não falo nada! Arriscado demais!", points: { timid: 2 } }
+        ]
+    },
+    {
+        text: "Você tá em uma caminhada quando uma reporter te aborad pra uma entrevista. O que você faz?", answers: [
+            { text: "Saio correndo!", points: { timid: 4 } },
+            { text: "Respondo as perguntas numa boa.", points: { brave: 4, sassy: 4 } },
+            { text: "Avacalho com ela. Tô na TV!", points: { bold: 2, naive: 4 } }
+        ]
+    },
+    {
+        text: "As pessoas na mesa do lado estão cantando Parabéns para alguém. O que você faz?", answers: [
+            { text: "Fico nos arredores", points: { calm: 2 } },
+            { text: "Canto junto!", points: { jolly: 4, naive: 4 } },
+            { text: "Vou embora.", points: { lonely: 2, sassy: 4 } }
+        ]
+    },
+    {
+        text: "Você vê uma cama grande e confortável. Sua primeira reação é ...", answers: [
+            { text: "Pular nela!", points: { hasty: 2, sassy: 2 } },
+            { text: "Bola de canhão!", points: { impish: 4 } },
+            { text: "Cair e dormir.", points: { lonely: 4 } }
+        ]
+    },
+    {
+        text: "Você tá sonhando acordado ... quando um amigo seu te molha todo! O que você faz?", answers: [
+            { text: "Fico puto!", points: { hasty: 4 } },
+            { text: "Fico triste.", points: { lonely: 4 } },
+            { text: "Aeee! Briga de água!", points: { impish: 4, jolly: 4, naive: 4 } }
+        ]
+    }
+];
---
+++ assets/results.js
@@ -0,0 +1,133 @@
+const descriptions = {
+    docile: [
+        "Você aparenta ser ... uma pesosa dócil. Você é gentil, prestativo e faz amigos com todos!",
+        "Você é uma pessoa maravilhosa.",
+        "......",
+        "Será que eu tô exagerando? Não acho. Você sabe isso melhor que eu.",
+        "Uma pessoa dócil como você deve ser o Pokémon ..."
+    ],
+    hardy: [
+        "Você aparenta ser ... uma pessoa resiliente. Você faz seu dever de casa e sabe comer direito.",
+        "Você tem muita força de vontade que te deixa completar tarefas, não importa quão difíceis.",
+        "Mas você também pode ser teimoso ao ponto de brigar com seus amigos ...",
+        "Nada vai dar certo pra você quando estiver irritado, então aprenda a esfriar a cabeça!",
+        "Uma pessoa resiliente como você deve ser o Pokémon ..."
+    ],
+    jolly: [
+        "Você aparenta ser ... uma pessoa feliz. Sempre rindo e sorrindo, você agrada todos ao seu redor.",
+        "Você adora piadas! Você tem muitos amigos e é popular por todo lugar que vai.",
+        "Mas às vezes você se deixa levar e diz coisas que te deixa em apuros.",
+        "Você devia aprender a pensar melhor antes de dizer ou pensar em alguma coisa.",
+        "Uma pessoa feliz como você deve ser o Pokémon ..."
+    ],
+    impish: [
+        "Você aparenta ser ... uma pessoa travessa. Você é brincalhão, alegre e adora pregar peças.",
+        "Você também é gentil. E é por isso que pessoas ao seu redor te acham irresístivel.",
+        "Você deve ser a pessoa mais popular do seu grupo!",
+        "Ah, você não é tão popular? Ou você está sendo modesto ... ou só não notou.",
+        "Aposto que as pessoas são tímidas demais pra mostrar seus sentimentos.",
+        "Tem alguém aí que tá com medo demais pra declarar o amor dele por você!",
+        "Uma pessoa travessa como você deve ser o Pokémon ..."
+    ],
+    quirky: [
+        "Você aparenta ser ... uma pessoa peculiar. Pessoas te consideram um ecêntrico que faz tudo do seu jeito.",
+        "Seu jeitão sem preocupações te faz atraente, mas também um pouco infantil ...",
+        "Você é meio volúvel e causa problemas para as pessoas ao seu redor.",
+        "Talvez até as pessoas estão chateadas por você ser tão pipa voada?",
+        "Se você ver que estiver agindo de maneira egoísta, tenta pensar antes de fazer algo drástico.",
+        "Uma pessoa peculiar como você deve ser o Pokémon ..."
+    ],
+    relaxed: [
+        "Você aparenta ser ... uma pessoa tranquila. Você ocasionalmente vai pro mundo da lua e perde o busão?",
+        "Ou você se vê dormindo do nada?",
+        "Ou o seu tempo de reação é um pouco menor que dos outros?",
+        "Mas isso não é necessariamente uma coisa ruim.",
+        "Você faz as coisas no seu tempo sem se sentir pressionado. Você pode viver de um jeito relaxado, sem pressa e sem preocupações.",
+        "Acho que é um jeito de viver feliz que é invejável, até. Você também é surpreendentemente popular.",
+        "O jeito que você olha pro nada, deve fazer o pulso de alguém especial começar a correr.",
+        "Uma pessoa tranquila como você deve ser o Pokémon ..."
+    ],
+    brave: [
+        "Você aparenta ser ... uma pessoa valente. Você tem um senso de justiça forte. Você odeia o mal. Você enfrenta qualquer um.",
+        "Você é um herói de verdade.",
+        "Vá lutar contra as forças das trevas!",
+        "Pela justiça ..., pela paz na terra ... vá!",
+        "Se eu estiver errado ... trabalhe para virar um herói de verdade!",
+        "Uma pessoa valente como você deve ser o Pokémon ..."
+    ],
+    lonely: [
+        "Você aparenta ser ... uma pessoa solitária. Você sempre parece alegre e brincalhona ao redor dos outros.",
+        "Mas é só porque você tá com os outros. Quando você fica sozinho ...",
+        "Você se sente estranhamente deprimida?",
+        "É por isso que você quer estar com os outros. Mas se você se sente muito deprimido ...",
+        "Sua saúde vai pro espaço. Coma mais vegetais!",
+        "Dito isso ... não é uma coisa ruim se sentir sozinho. Você sabe como é quando se não está sozinho e é por isso que fica triste.",
+        "E é por isso que não está solitário de verdade.",
+        "Uma pessoa solitária como você deve ser o Pokémon ..."
+    ],
+    timid: [
+        "Você aparenta ser ... uma pessoa tímida. Você pode achar difícil ir ao banheiro à noite.",
+        "Ou voltar pra escola para pegar algo que você esqueceu na sala.",
+        "Se você estiver andando em uma rua escura à noite, você provavelmente olha pra trás frequentemente.",
+        "Mas sua natureza tímida também é seu ponto bom!",
+        "Porque aqueles que conhecem o medo também conhecem a verdadeira coragem.",
+        "Uma pessoa tímida como você deve ser o Pokémon ..."
+    ],
+    naive: [
+        "Você aparenta ser ... uma pessoa ingênua. Você é bem curioso e adora coisas raras.",
+        "Sua atitude alegre e despreocupada deve fazer coisas divertidas pras pessoas ao seu redor.",
+        "Mas você tem uma falha: você pode ser infantil.",
+        "Você nunca fica quieto. Mas sempre está se mexendo.",
+        "Você também pode ser egoísta, então tome cuidado!",
+        "Uma pessoa ingênua como você deve ser o Pokémon ..."
+    ],
+    sassy: [
+        "Você aparenta ser ... uma pessoa atrevida. Você é um pouco cínico.",
+        "Mas apesar disso tem algo atraente e amável em você.",
+        "Mas você ocasionalmente diz algo arrogante que irrita os outros?",
+        "Você faz esse tipo de erro? Pessoas já te chamaram de vão, egoísta ou convencido?",
+        "Pessoas já disseram isso pra você?",
+        "Ah? Tá me mandando ir tomar no cú?",
+        "Vai você, seu filho da puta.",
+        "Ah. Desculpa. Deixei me levar. Mil perdões, de verdade.",
+        "De qualquer forma sua atitude é o que te define.",
+        "Te faz irritante e agradável ao mesmo tempo.",
+        "Uma pessoa atrevida como você deve ser o Pokémon ...",
+    ],
+    hasty: [
+        "Você aparenta ser ... uma pessoa apressada. Você gosta de ir a frente e fazer as coisas.",
+        "Você é bem motivado.",
+        "Mas também é bem estressado?",
+        "Você fica irritado quando seus amigos não chegam na hora.",
+        "Você fica frustrado quando coisas não acontecem como você espera.",
+        "Você bate no botão do elevador se ele demora pra chegar.",
+        "Talvez você só tenha apertado o botão \"Enviar\" sem parar agora.",
+        "Toma cuidado - ficar irritado com facilitada não é bom pro seu bem-estar.",
+        "Uma pessoa apressada como você deve ser o Pokémon ...",
+    ],
+    calm: [
+        "Você aparenta ser ... uma pessoa calma. Você é capaz de dar conselhos aos seus amigos sem problemas.",
+        "Você nãog sota de lutar.",
+        "Você é uma pessoa gentil que se importa.",
+        "Você tem muitos amigos que se modelam em você.",
+        "Mas ... você também é um pouco ingênuo ... e relaxado ... e talvez descuidado.",
+        "Você talvez queira ter essas coisas em mente.",
+        "Uma pessoa calma como você deve ser o Pokémon ..."
+    ]
+}
+
+const starters = {
+    docile: { mon: "Happiny", pt: "dócil" },
+    hardy: { mon: "Aron", pt: "resiliente" },
+    jolly: { mon: "Woobat", pt: "feliz" },
+    impish: { mon: "Sprigatito", pt: "travesso" },
+    quirky: { mon: "Trubbish", pt: "peculiar" },
+    relaxed: { mon: "Slowpoke", pt: "tranquila" },
+    brave: { mon: "Scraggy", pt: "valente" },
+    lonely: { mon: "Cubone", pt: "solitária" },
+    timid: { mon: "Snom", pt: "tímida" },
+    naive: { mon: "Pichu", pt: "ingênua" },
+    sassy: { mon: "Litten", pt: "atrevida" },
+    hasty: { mon: "Growlithe", pt: "apressada" },
+    calm: { mon: "Munchlax", pt: "calma" }
+};
